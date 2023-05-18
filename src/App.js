59af70df1c5a48441370515289e1780e4810979f
MarcosNovella <marcosnovella99@gmail.com>
2023-05-18 19:27:14 -0300

    deleting toastify imports

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import { Route } from "react-router-dom/cjs/react-router-dom.min";
 import Home  from './components/Home/Home';
 import Detail from "./components/Detail/Detail";
 // import landing from './components/landing/landing';
-import { ToastProvider } from 'react-toast-notifications';
-import 'react-toastify/dist/ReactToastify.css';
+// import { ToastProvider } from 'react-toast-notifications';
+// import 'react-toastify/dist/ReactToastify.css';
 import Register from "./components/Form/Register";
 
 axios.defaults.baseURL="http://localhost:3001/";
 
 function App() {
   return (
-    <ToastProvider>
+    // <ToastProvider>
       <div>
           <Route exact path = '/' render = {() =><Home /> } />
           {/* <Route path = '/login' render = {() => <Form /> } /> */}
           <Route path = '/detail/:id' render = {() =>  <Detail/>} />
           <Route path = '/register' render = {() => <Register />} />
       </div>
-    </ToastProvider>
+    // </ToastProvider>
   );
 }
 
