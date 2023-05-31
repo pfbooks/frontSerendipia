d7e101c9c2888e771b066ff2faba2bf5cc900116
LautaroUnlp <kevinlautarosoto@gmail.com>
2023-05-30 22:24:46 -0300

    add some styles to NavBar and Success

--- src/components/Payment/Success.jsx
+++ src/components/Payment/Success.jsx
@@ -1,34 +1,37 @@
 import React, { useEffect } from "react";
 import { FaCheckCircle } from "react-icons/fa";
 import styles from "./Success.module.css";
-import { useCart } from "../../hooks/useCart"
+import { useCart } from "../../hooks/useCart";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Success = () => {
   const { cart, clearCart } = useCart();
-  const user = JSON.parse(window.localStorage.getItem("user"))
+  const user = JSON.parse(window.localStorage.getItem("user"));
   const obj = {
     userId: user.id,
-    books: cart
+    books: cart,
   };
-  
+
   useEffect(() => {
-    const axiosFunc = async() =>{
-      await axios.post('/order/add', obj)
-      .then(res => console.log(res))
-      .catch(error => console.log(`error del post: ${error}`))
+    const axiosFunc = async () => {
+      await axios
+        .post("http://localhost:3001/order/add", obj)
+        .then((res) => console.log(res))
+        .catch((error) => console.log(`error del post: ${error}`));
+    };
+    console.log(obj);
+    axiosFunc();
 
-    }
-    console.log(obj)
-    axiosFunc()
-
-    clearCart()
-
+    clearCart();
   }, []);
   return (
     <div className={styles.container}>
       <FaCheckCircle className={styles.icon} />
       <h1 className={styles.title}>¡Pago Exitoso!</h1>
+      <Link to={`/shop/${user.id}`}>
+        <button className={styles.buttonLink}>My Shop</button>
+      </Link>
     </div>
   );
 };
